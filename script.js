c809a73ab076cbb49982a7840cc2c9c8995a534d
web-portfolio <achmadzaki10d@gmail.com>
2022-10-22 20:23:02 +0700

    fix bug

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const year = document.getElementById("year").innerHTML = yyyy
 
 window.addEventListener('scroll', function() {
     const nav = document.querySelector("#navbar");
-    if (window.pageYOffset > 50) {
+    if (window.pageYOffset > 10) {
         nav.classList.add('bg-custom-primary', 'shadow');
     } else {
         nav.classList.remove('bg-custom-primary', 'shadow');
